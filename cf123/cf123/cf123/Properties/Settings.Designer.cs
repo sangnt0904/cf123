91d8e875ad763b338d83e9fb10b48b30d6586086
Nguyễn Thanh Sang <sangnt0904@gmail.com>
2018-01-07 13:40:49 +0700

    server

--- cf123/cf123/cf123/Properties/Settings.Designer.cs
+++ cf123/cf123/cf123/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cf123.Properties
-{
-
-
+namespace cf123.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=localhost;user id=root;password=usbw;persistsecurityinfo=True;database=cof" +
+            "feebusiness2")]
+        public string coffeebusiness2ConnectionString {
+            get {
+                return ((string)(this["coffeebusiness2ConnectionString"]));
             }
         }
     }
